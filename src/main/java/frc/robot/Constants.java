5220ff8fd883c509a330c56c1143c164525a8208
kentbraaten <kentbraaten@frontiernet.net>
2025-02-09 16:13:00 -0600

    Stripped out all but drivetrain.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -55,30 +55,10 @@ public final class Constants {
   public static final double WHEEL_REVOLUTIONS_PER_METER = 3.0;
   public static final double PERCENTAGE_MAX_SPEED = 100.0;
 
-  public static final int LIGHT_ID = 31;
-
-  public static final int LAUNCHER_WHEEL_FRONT=51;
-  public static final int LAUNCHER_WHEEL_BACK=50;
-
-  // Intake constants
-    public static final int INTAKE_MOTOR_CAN_ID = 15;
-    public static final double INTAKE_MOTOR_SPEED = 1;
-
-  // Arms constants
-    public static final int LEFT_ARM = 40;
-    public static final int RIGHT_ARM = 41;
-
-  // Launcher constants
-  // TODO: Add launcher constants
 
   //Make stuff stop working so errors dont make the drivers whine
-    public static final boolean DISABLE_INTAKE = false;
     public static final boolean DISABLE_DRIVETRAIN = false;
-    public static final boolean DISABLE_CANDLE = false;
-    public static final boolean DISABLE_LAUNCHER = false;
-    public static final boolean DISABLE_RUMBLE=false;
-    public static final boolean DISABLE_ARMS=false;
-    public static final boolean DISABLE_POSE_ESTIMATOR=false;
+    public static final boolean DISABLE_POSE_ESTIMATOR=true;
 
   //stop blinding kent
     public static final int BRIGHTNESS_DIVISOR = 10;
@@ -89,52 +69,3 @@ public final class Constants {
     public static Transform3d CAMERA_TO_ROBOT = new Transform3d(0,0,0,new Rotation3d());
     public static Transform3d ROBOT_TO_CAMERA = new Transform3d(0,0,0,new Rotation3d());
 }
-
-/*Whohahahao
-                                           :!?JY55Y?!^
-                                          ^YGGGGGBBBGP?:
-                              :::::::::~7!YGGGGBBBBBBBB!
-                     :^^^!!!7777??77YPB#&#GBBBGGBBBBBB#5
-                   ^7??JYGGGGGGGP55PB&&&&BBBGPPGPGGGGBB7
-                   ?GPGGGBBBBBBGGGGB&&&BPGGBGPPPPPPPG##J~^:
-                  :JPPPPBBBBBBBBGGG#&#PY55GBBGGGGGGPGBBGYJJJ7~^
-            ::^~!7?5PPPGBBBBBB#BGGG##5PPPGGBBBBBBGGGBBGGGPBBBGP5?:
-         :~!!77?JYYPGGGG#######BGGG#BGGPPGBB#BBBBBBBBGPGGB#BBB##GY7~:
-      ^~?J?????7?YYGGGGB&&&####BBBBBBBGPPPGBB#BGGGBBG5GGBBBBB##GGGGP5?:
-   :!?YYYJ!?JJJ?7?5GY?J5PGB##&&#BB#GGGGPPPGBB#BBBBBB5PBBBBGB#BPPPPGB5~:~!!~!???7:
- :!J55YYYJ7??????JPJ!?55YYY55YPGBB#BGPPPGGBBBBGGGGP5GB####B#BPPPPGPP!:75PGPPGG5?^
-!Y555555YYYYJJJY5GBGJ5PP5555?!YGGB#BGGGGPPPPPPPPPPPB#&&&&&&#GGGGG5YYJ77!~^:^YGGGGGGPPPP5?:
-PP55555YYY5555PGGP5?7Y5PPPPP?YGBGGGGGGGGGGGGGGGGGGBGGGBBBBBGPGGGPPPPP55YYY?!!~^:^^^^YGGGGGP5GBBBGBP!
-?5PPPP55555PGGPJ~^J75555555J?5GBGGGGGGGGGGGGGGGGBG55?7Y55555Y?5BGGGGGPP555Y5Y7!!!!!!PBGGGPPPPGGGBBBP
-:!77?JJY555PP!:  ~B555PPPPJ!YGBBBBBBBBBBBBBBBBBGGP5PY?5PPPPPPYP&#BBBGGGPPP5PJ7777777JPGGBGGBGPP5Y?!^
- ~!!!!!!!!!7?7:  ^J7?Y55PG55GBB##BBBBBBBB#####G55?JP55PGGP5GJ!~JPB###BGGGPPPY????JJYY55555Y7^::
- ^7!!!!!!!!!!7?: :7J555YY5Y?YG###############BP5PJJPPP555YJY:    ^!YGB#BBGPPPYJYYY5555555?^
-  !7!!!!!!!!!!?~  !BGGPP5Y!?5G##B###########PY?YP5PGGGGPGPGY:       :^7YGBBGGP55PPPP55J!:
-  :!7!!!!!!!!!77: ^5BBBBBPYGB#&BY555555555555PJ?5PPPPP5YG#Y:            :~YPPPPPPPY7~:
-   :!!!!!!!!!!!7~:PGGBBBBB###&&#55555PPP55555PPPGGPPPPPB#?                 :::^^~^:
-    ^!!!!!~~~~~!7JGGGBBGBB#####B555PPGGGP55555PGGGP555G#P::
-     :!!!~~~~!7??JPGB#YYGGBB####555PPGGGPP555PPGGGPP55555J?7!^:
-      :!!~!?Y5PPP55PGYJ5555PPGBB5555PPPPP55555PPPPP555PPPP55YYJ?7~:
-       :?5PPPPPPPPPPPPYYY55PPPGG5555555555555555555555PPPPPPPPPPPP5J!^
-        ~PPPPPPPPPPGGGG5Y5PPGBBB5555555555555555555555PPPPPPPPPPPPPGGPY7^:
-         ?PPPPPGGPGBGGGGPPPGBBBBPPPPPPPPPPPPPPPPPPPPPPPPPPPPGGGGGGPGGGGG5J~
-         :YGGPGBBGGGGGGGGGPGBBBBBBBBBBBBBBBBBBBBBBBBBBGGGPPPGGGGGGGGGGGGBG57
-          JGGGGGGPGGGGBBGBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB#BBBBBBBGGGGGGGGBB#B5:
-        :!JPGPGGGPPGBGBB##BBBBBBBBBBBBBBBBBBBBBBBBBG!!7?JY5PP55GGGGGGGGB##5?~
-      :!J55PPPPGGGGG#B#BY!^~~YBBBBBBBBBBBBBBBBBBBBB!         ^YPGGGGGGGGBBBP7:
-    :~?Y5PPPPGGGGBBB#G7:     :YBBBBBBBBBBBBBBBBB#G7        ^7PGGGGGGGGGGBBGGP^
-   ~?Y5PGGGGGGGBB##G?:         ~5BBBBBBBBBBBBB#B?:         ?GGGGGGGGGBBBGGBB?
- :755PGGGGGGGBBBBG?:             !5B#BBB#####BJ^           ^JGGGGGGGGGGGGBG~
- ?PPPPGGGGGGGBB#BY:                !YYJJJJJJJ~              JBBGGGGGGB###5:
- ^YGGGGGGGGBBBBBBBJ                                        :GBBBBB##5?JY?
-  :YBGGGGGBBBBBBBB5:                                       :PBBBB#&5:
-    JBGGGGGGGGGBP!:                                      :!JBBBB#&5:
-     755PBBBBB#B5:                                       7PGGGBB##7
-        JBB##BB##!                                      !PGGGGGGG#Y
-        ?GGGBBB#P:                                     :JGGGGGGGBB~
-       ~PGGGGGB#P:                                      ?GGGGGGGBP:
-       75GGGGGBBBJ                                     ^GBGGGGGGGG5~
-   ^~~!75PGBBBBB#G:                                    ?BBBBBBGBBGY!:
-  ?GBBBBBBGGGBB##B?                                    YBBBBBBBBBGGP55555Y^
- ~PGGGGGGGGGGBPPP5?:                                  !PGGBBGGGGGGGBBBGGGBY^
- ^7??????7777!:                                       ~77?JYJJYY555PPPPPPGP~*/
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,34 +5,15 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.BooleanFunc;
-import frc.robot.commands.Lighter;
-import frc.robot.commands.MoveArms;
-import frc.robot.commands.MoveArmsUntilResistance;
-import frc.robot.commands.MoveToPosition;
 import frc.robot.commands.ResetIMU;
-import frc.robot.commands.Rumbly;
-import frc.robot.commands.SavePosition;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import frc.robot.commands.SpinUpLauncher;
 import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.commands.DriveFieldRelative;
-import frc.robot.commands.FeedNoteToLauncher;
-import frc.robot.commands.IndicateNote;
-import frc.robot.commands.IntakeReverse;
-import frc.robot.commands.IntakeReverseRegardless;
-import frc.robot.commands.IntakeStart;
-import frc.robot.commands.LaunchNote;
-import frc.robot.subsystems.Arms;
 import frc.robot.subsystems.DriveTrain;
-import frc.robot.subsystems.Lights;
 import frc.robot.subsystems.PoseEstimatorSubsystem;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Launcher;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,9 +25,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -71,16 +50,8 @@ public class RobotContainer {
   ResetIMU resetIMUCommand;
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private DriveTrain drivetrain;
-  private Lights candle;
-  private IndicateNote notelight;
-  private Intake intake;
   private PoseEstimatorSubsystem poseEstimatorSubsystem;
-  private IntakeStart intakestart;
-  private Launcher launcher;
-  private Arms arms;
-  private MoveArms moveArms;
   private SendableChooser<Command> mailman;
-  private Supplier<Pose2d> wheretogo;
   private TrajectoryCommandFactory trajectoryCommandFactory;
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
@@ -97,12 +68,7 @@ public class RobotContainer {
 
     // Configure the trigger bindings
     configureDriveTrainBindings(Constants.DISABLE_DRIVETRAIN);
-    configureIntakeBindings(Constants.DISABLE_INTAKE);
-    configureCandleBindings(Constants.DISABLE_CANDLE || Constants.DISABLE_INTAKE);
-    configureLauncherBindings(Constants.DISABLE_LAUNCHER);
     configurePoseEstimatorSubsystem(drivetrain);
-    configureArms(Constants.DISABLE_ARMS);
-    configureCompoundCommands(Constants.DISABLE_INTAKE || Constants.DISABLE_LAUNCHER);
     this.trajectoryCommandFactory = new TrajectoryCommandFactory(drivetrain, poseEstimatorSubsystem);
     populateMailbox();
      SmartDashboard.putData("autonomous modee", mailman);
@@ -140,36 +106,6 @@ public class RobotContainer {
     return Math.copySign(input * input, input);
   }
 
-  private void configureIntakeBindings(boolean disabled) {
-    if (disabled) {
-      System.out.println("Disabled the Intake system.");
-    } else {
-      this.intake = new Intake();
-      this.intakestart = new IntakeStart(intake,Constants.INTAKE_MOTOR_SPEED);
-      m_cmdcontroller.leftBumper().toggleOnTrue(
-        intakestart.andThen(
-           new Rumbly(m_controller).withTimeout(0.5))
-           .andThen(new IntakeReverse(intake))
-        .andThen(new IntakeStart(intake, 1))
-        .andThen(new IntakeReverse(intake))
-        .andThen(new IntakeStart(intake,1))
-        .andThen(new IntakeReverse(intake))
-        .andThen(new IntakeStart(intake,0.3)));
-      m_cmdcontroller.back().whileTrue(new IntakeReverseRegardless(intake));
-    }
-  }
-
-  private void configureLauncherBindings(boolean disabled) {
-    if (disabled) {
-      System.out.println("Disabled the launcher subsystem");
-      return;
-    }
-    launcher=new Launcher();
-    m_cmdcontroller.start().toggleOnTrue(new LaunchNote(launcher));
-
-
-  }
-
   private void configureDriveTrainBindings(boolean disabled) {
     if (disabled) {
       System.out.println("Disabled the DriveTrain system.");
@@ -198,45 +134,6 @@ public class RobotContainer {
   }
 
 
-
-  private void configureCandleBindings(boolean disabled) {
-    if (disabled) {
-      System.out.println("Disabled the CANdle system.");
-    } else {
-      this.candle = new Lights();
-      this.notelight = new IndicateNote(candle, ()->intake.isLimitSwitchEngaged());
-
-    }
-  }
-
-  private void configureArms(boolean disabled) {
-    if (disabled) {
-
-    } else {
-      arms = new Arms();
-      moveArms = new MoveArms(arms, m_controller);   
-      
-      arms.setDefaultCommand(moveArms);
-
-    }
-  }
-
-  private void configureCompoundCommands(boolean disabled) {
-    if (disabled) {
-      System.out.println("Disabled compound commands");
-    }
-
-    m_cmdcontroller.b().onTrue(new SpinUpLauncher(launcher,intake,4000,3500,true,false).withTimeout(8)
-      .andThen(new FeedNoteToLauncher(intake).alongWith(new SpinUpLauncher(launcher,intake,4000,3500,false,true)).withTimeout(8).andThen(new Rumbly(m_controller).withTimeout(0.3))));
-
-    m_cmdcontroller.a().onTrue(new SpinUpLauncher(launcher,intake,3500,2500,true,false).withTimeout(8)
-      .andThen(new FeedNoteToLauncher(intake).alongWith(new SpinUpLauncher(launcher,intake,3500,2500,false,true)).withTimeout(8).andThen(new Rumbly(m_controller).withTimeout(0.3))));
-    
-      // TODO: Bind a command to the right bumper that:
-    // 1. Runs LaunchNote for .5 secons.
-    // 2. Runs FeedNoteToLauncher and LaunchNote togeter for 2 seconds
-  }
-
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
@@ -246,45 +143,18 @@ public class RobotContainer {
 
   private void populateMailbox()
   {
-    if (Constants.DISABLE_LAUNCHER) {
-      System.out.println("Disabled mailbox because launcher is disabled.");
-      System.out.println("WE WOULDNT WANT ANY FATAL ERRORS, WOULD WE ETHAN?");
-      System.out.println("*cough cough*");
-    } else {
       mailman=new SendableChooser<>();
      // Command intakeCommand=new IntakeStart(intake,1).andThen(new IntakeReverse(intake).andThen(new IntakeStart(intake, 0.3)));
      // Command launchCommand=new SpinUpLauncher(launcher,6000,6000,true).withTimeout(4).andThen(new SpinUpLauncher(launcher,6000,6000,false).alongWith(new FeedNoteToLauncher(intake))).withTimeout(4);
       Command move=new DriveFieldRelative(drivetrain, 0, 0.25);
       //mailman.setDefaultOption("launch",intakeCommand.andThen(launchCommand));
-      mailman.addOption("launch",
-        new IntakeReverse(intake)
-        .andThen(new IntakeStart(intake, 0.3)
-        .andThen(new SpinUpLauncher(launcher,intake,6000,6000,true,false))
-        .andThen( (new SpinUpLauncher(launcher,intake,6000,6000,false,true).alongWith(new FeedNoteToLauncher(intake))).withTimeout(6) )
-          ));
-      mailman.addOption("launch+move",
-        new IntakeReverse(intake)
-        .andThen(new IntakeStart(intake, 0.3)
-        .andThen(new SpinUpLauncher(launcher,intake,6000,6000,true,false))
-        .andThen( (new SpinUpLauncher(launcher,intake,6000,6000,false,true).alongWith(new FeedNoteToLauncher(intake))).withTimeout(1) )
-        .andThen(move.withTimeout(2))
-          ));
-        mailman.addOption("launch+move+intake",
-        new IntakeReverse(intake)
-        .andThen(new IntakeStart(intake, 0.3)
-        .andThen(new SpinUpLauncher(launcher,intake,6000,6000,true,false))
-        .andThen( (new SpinUpLauncher(launcher,intake,6000,6000,false,true).alongWith(new FeedNoteToLauncher(intake))).withTimeout(1) )
-        .andThen(new DriveFieldRelative(drivetrain, 0, 0.25).alongWith(new IntakeStart(intake,1)).withTimeout
-        (2))));
       mailman.addOption("back", new DriveFieldRelative(drivetrain, 0, 0.15));//.withTimeout(15));
       mailman.addOption("nothing", null);
-      mailman.addOption("DO NOT USE ON FIELD!!!!! arm retract (for the pit)",new MoveArmsUntilResistance(arms, m_controller));
 
       mailman.addOption("move with waypoints", createTrajectoryCommandWithWayPoints());
       mailman.addOption("move without waypoints", createTrajectoryCommandNoWayPoints());
       mailman.addOption("move with waypoints starting from diff location", createTrajectoryCommandStartDiffThanRobotPosition());
       SmartDashboard.putData("autonomous mode", mailman);
-    }
   }
   
   public Command getAutonomousCommand(){
